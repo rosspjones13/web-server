ed5759f33e65a8c38ecaacc16ed650afba1dba16
Ross Jones <rossmosis.jones@gmail.com>
2019-06-17 09:26:01 -0400

    inital commit

---
+++ src/app.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public/')
+const viewsPath = path.join(__dirname, '../templates/views/')
+const partialsPath = path.join(__dirname, '../templates/partials/')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Ross Jones'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    name: 'Ross Jones'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    helpMessage: 'Helpful text',
+    name: 'Ross Jones'
+  })
+})
+
+app.get('/weather', (req, res) => {
+  res.send({
+    location: 'Philadelphia',
+    weather: '50deg'
+  })
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Help article not found',
+    name: 'Ross Jones'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    errorMessage: 'Page not found',
+    name: 'Ross Jones'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('server is up')
+})
