3eced9ad6868dd3d455d8de14cf243d15999fe3b
Ross Jones <rossmosis.jones@gmail.com>
2019-06-18 15:57:37 -0400

    updating api key location

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,6 @@
 const request = require('request')
+const config = require('../data/config.js')
+const DARKSKY_KEY = process.env.DARKSKY_KEY || config.DARKSKY_KEY
 
 const forecast = (latitude, longitude, callback) => {
   const url = `https://api.darksky.net/forecast/${DARKSKY_KEY}/${latitude},${longitude}?units=us`
