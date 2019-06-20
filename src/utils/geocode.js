9d3de04e15606ba87254519146e8567bffb163e8
Ross Jones <rossmosis.jones@gmail.com>
2019-06-20 13:27:20 -0400

    setting up app for heroku after revert

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request')
-const config = require('../data/config.js')
-const MAPBOX_KEY = process.env.MAPBOX_KEY || config.MAPBOX_KEY
+// const config = require('../data/config.js')
+const MAPBOX_KEY = process.env.MAPBOX_KEY
 
 const geocode = (address, callback) => {
   const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${MAPBOX_KEY}`
